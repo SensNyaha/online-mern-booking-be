f3df2944d1b47bc3e113c9bae22d8bac9a39abeb
sensnayah <mwmwakarov@vk.ru>
2024-04-05 21:31:09 +0400

    added register post request

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-import express from 'express';
-import 'dotenv/config';
-import morgan from 'morgan';
+import express from "express";
+import "dotenv/config";
+import morgan from "morgan";
 import cors from "cors";
 
 import authRouter from "./routes/auth.js";
-import connectToDb from './helpers/connectToDb.js';
+import registerRouter from "./routes/register.js";
+import connectToDb from "./helpers/connectToDb.js";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(cors());
+app.use(express.json());
 
-app.use('/api/auth', authRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/register", registerRouter);
 
 app.get("/api/", (req, res) => {
     res.send("Hello");
-})
+});
 
 const PORT = process.env.PORT || 3001;
 
-connectToDb()
-    .then(() => {
-        console.log("Server connected to DB on address: " + process.env.MONGO_URL)
-        app.listen(PORT, () => {
-            console.log(`Server started on port: ${PORT}`)
-        })
-    })
-
+connectToDb().then(() => {
+    console.log("Server connected to DB on address: " + process.env.MONGO_URL);
+    app.listen(PORT, () => {
+        console.log(`Server started on port: ${PORT}`);
+    });
+});
